f24343c5b267e60b7671e4128f75c0ee67f16953
Sergej Galkin <13518510+galkin-it@users.noreply.github.com>
2022-06-21 05:11:28 +0200

    Update homework.py

--- homework.py
+++ homework.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def main():
     #fileName = input("Enter csv filename: ")
-    fileName = "/Users/gas1lb/privat/gitlab/ukraine/tanja/homework/14005_EP2500A_Monat_02_18.csv"
+    fileName = "CSV_File.csv"
     content = pd.read_csv(fileName, "rb")
     
     print(content)
